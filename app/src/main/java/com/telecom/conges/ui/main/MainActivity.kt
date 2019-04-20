787fd543cd7ed6d82de45986cca32c9e94585bb5
Selim <sloumma.21@gmail.com>
2019-04-20 13:04:47 +0100

    Create MainActivity Dashboard

--- app/src/main/java/com/telecom/conges/ui/main/MainActivity.kt
+++ app/src/main/java/com/telecom/conges/ui/main/MainActivity.kt
@@ -2,15 +2,39 @@ package com.telecom.conges.ui.main
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.telecom.conges.R
+import com.telecom.conges.ui.account.AccountActivity
+import com.telecom.conges.ui.history.HistoryActivity
+import com.telecom.conges.ui.request.RequestActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun handleClick(view: View) {
+        when (view.id) {
+            R.id.historique -> {
+                HistoryActivity.starterIntent(this@MainActivity)
+            }
+            R.id.request -> {
+                RequestActivity.starterIntent(this@MainActivity)
+
+            }
+            R.id.account -> {
+                AccountActivity.starterIntent(this@MainActivity)
+
+            }
+            else -> {
+                Log.v("UNHANDLED ", "Clicked has no implementation Yet")
+            }
+        }
     }
 
     companion object {
